5738a936f21c7cbebfc2b4c32ca307cb2bbd87ec
Pablo de Andrade Silva <pablodeas@gmail.com>
2024-06-26 18:07:20 -0300

    version:0.0.1 finished

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import json, sys, argparse, psycopg2, os
+import psycopg2, os, click
 from datetime import datetime
 from dotenv import load_dotenv
 
@@ -17,11 +17,11 @@ def list():
                 rows = cur.fetchall()
 
                 for i in rows:
-                    print(f"> Id:{i[0]} Value: R${i[1]} | Date: {i[2]} | Descr: {i[3]}")
+                    print(f"> Id:{i[0]} | Value: R${i[1]} | Date: {i[2]} | Descr: {i[3]}")
                 
                 #conn.commit()
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"> An error occurred: {e}")
 
 def insert(value, desc):
     try:
@@ -37,13 +37,73 @@ def insert(value, desc):
                 conn.commit()
                 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"> An error occurred: {e}")
+
+def delete(id):
+    try:
+        with psycopg2.connect(dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=int(PORT)) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                    DELETE FROM public.register where Id = %s;
+                    """,
+                    (id))
+                conn.commit()
+
+    except Exception as e:
+        print(f"> An error occurred: {e}")
+
+def clear():
+    try:
+        case = input("> WARNING - You will clear all values. Are you sure? (Y)-Yes or (N)-No\n> ")
+        
+        if case == "Y":
+            with psycopg2.connect(dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=int(PORT)) as conn:
+                with conn.cursor() as cur:
+                    cur.execute("""
+                        DELETE FROM public.register;
+                        """)
+                    conn.commit()
+                    print("> All clear.")
+        else:
+            pass
+
+    except Exception as e:
+        print(f"> An error occurred: {e}")
+
+def check():
+    try:
+        with psycopg2.connect(dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=int(PORT)) as conn:
+            with conn.cursor() as cur:
+                cur.execute("select sum(Value) from public.register")
+                sum = cur.fetchone()
+                if sum[0] is not None or Null:
+                    sum_value = sum[0]
+
+                cur.execute("select const from public.week")
+                week = cur.fetchone()
+                if week[0] is not None or Null:
+                    week_value = week[0]
+                
+                left = week_value - sum_value
+                print(f"> Spend: R${sum_value} Week: R${week_value} ->> Left: R${left}")
+                
+
+    except Exception as e:
+        print(f"> An error occurred: {e}")
 
 def main():
     list()
-    #value = int(input("> Digite o Valor Gasto: \n"))
-    #descr = input("> Digite a Descrição: \n")
+    
+    #value = int(input("> Value: \n> "))
+    #descr = input("> Descr: \n> ")
     #insert(value, descr)
+    
+    #id = input(f"> Which Id: \n> ")
+    #delete(id)
+
+    clear()
+
+    #check()
 
 if __name__ == "__main__":
     main()
--- src/scripts/22-06-2024.sql
+++ src/scripts/22-06-2024.sql
@@ -1,11 +1,11 @@
 -- Primary Tables
 CREATE TABLE IF NOT EXISTS Register(
-    Id int primary key,
+    Id SERIAL primary key,
     Date DATE not null,
     Description varchar(50) not null,
     Value int not null
 );
 CREATE TABLE IF NOT EXISTS Week(
-	Id int primary key,
+	Id SERIAL primary key,
 	Const int not null
 );
